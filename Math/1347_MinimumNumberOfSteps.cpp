f86b6a6f7613e52883e02e0981f697f9d7bdd355
Nhat Ho <honhattan2411@gmail.com>
2020-02-09 14:15:17 -0800

    Fixing Indentation

--- Math/1347_MinimumNumberOfSteps.cpp
+++ Math/1347_MinimumNumberOfSteps.cpp
@@ -32,23 +32,23 @@ public:
         int count = 0;
         for(auto x:t) mapt[x]++;
         for(auto x:s) maps[x]++;
-		for(int i = 0; i < 26; i++){
-			char c = i + 'a';
-			count += abs(mapt[c]-maps[c]);
-		}
-		return count/2;
-	}
+        for(int i = 0; i < 26; i++){
+            char c = i + 'a';
+            count += abs(mapt[c]-maps[c]);
+        }
+        return count/2;
+    }
 };
 
 /*Find the different characters of 2 strings using vector*/
 class Solution {
 public:
     int minSteps(string s, string t) {
-		int alpha[26] = {};
+        int alpha[26] = {};
         int count = 0;
         for(auto x:t) alpha[x-'a']++;
         for(auto x:s) alpha[x-'a']--;
-		for(int i = 0; i < 26; i++) count += abs(alpha[i]);
-		return count/2;
-	}
+        for(int i = 0; i < 26; i++) count += abs(alpha[i]);
+        return count/2;
+    }
 };
