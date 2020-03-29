af4421322e2b6807a540adfd482a8448669e2359
Nhat Ho <honhattan2411@gmail.com>
2020-03-28 18:20:01 -0700

    Updating DP solution for problem 647

--- stringArray/647_PalindromicSubstrings.cpp
+++ stringArray/647_PalindromicSubstrings.cpp
@@ -1,3 +1,5 @@
+/*Expand Around Center
+ * Time Complexity O(n^2) Space O(1)*/
 class Solution {
 public:
     void checkPalindrome(string s, int left, int right, int& count){
@@ -17,5 +19,27 @@ public:
             checkPalindrome(s,i,i+1,count);
         }
         return count;
+    }
+};
+
+/*Using another appoarch by DP
+ * Time Complexity O(n^2) Space Complexity O(n^2)*/
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int len = s.length(), count = 0;
+        vector<vector<int>>temp(len, vector<int>(len,0));
+        for(int n = 1; n <= len; n++){
+            for(int i = 0, j = n+i-1; i <= len-n; i++, j++){
+                if(i == j) temp[i][j] = 1;//Updating for diagonal
+                else if(s[i] == s[j]){
+                    if(temp[i+1][j-1]== n-2){
+                        temp[i][j] = n;
+                        count++; //counting the palindrome substring
+                    }else temp[i][j] = 1;
+                }else temp[i][j] = max(temp[i+1][j], temp[i][j-1]); //updating DP table
+            }
+        }
+        return count+len;
     }
 };
