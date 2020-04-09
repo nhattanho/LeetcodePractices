2a8d98c66936a8351aca426dd0eea28c60b9e2c9
Nhat Ho <honhattan2411@gmail.com>
2020-04-08 22:04:27 -0700

    Adding explanation for 433 Minimum Genetic Mutation

--- Math/433_MinimumGeneticMutation.cpp
+++ Math/433_MinimumGeneticMutation.cpp
@@ -17,16 +17,16 @@ public:
                     word[i] = x;
                     it = find(bank.begin(), bank.end(), word);
                     if(it != bank.end()){
-			//Found the mutation ==> saving for the next checking
-			//If we have two mutation, first is A and then is B.
-			//Assume both of them have the same target "end",
-			//that means A or B has just only one different
-			//character with end,==> with this checking operation,
-			//A has to have the shorter path to reach the target
-			//than B, so that why we're using queue to save them.
-			//As the result, in the next checking, A comes to first
-			//and we will have the shortest path to the end or we
-			//have the minimum genetic mutation. 
+			/*Found the mutation ==> saving for the next checking*/
+			//If we have two mutation, first is start->A and the
+			//second is start->B.Because A and B are different, and
+			//they also have only one different character with the
+			//start gene, so A and B have to have at 2 different
+			//character.Now, we need to find how many mutation from
+			//A->end and B->end. To find the minimum of mutations, we
+			//do the sequential operation for A and B. Whenever
+			//A or B reaches the end, the minimum of mutations will base
+			//on the number of step from A or B to the end.
                         qsave.push({word,pset.second+1});
                         bank.erase(it);
                     }
@@ -37,3 +37,33 @@ public:
         return -1;
     }
 };
+/* For example:
+ * start: AACCGGTT
+ * end  : AACCGGCA
+ *
+ * bank : AACCGGTA, AACCGCTT, AACCGGCA, AACCGCTA, AACCGCCA
+ *
+ * first:  start = AACCGGTT -> AACCGCTT (A)
+ *                      `           `
+ * second: start = AACCGGTT -> AACCGGTA (B)
+ *                        `           `
+ * Continue checking for A and B, we will have:
+ * A: AACCGCTT -> AACCGCTA (C)
+ *
+ * B: AACCGGTA -> AACCGCTA (C)
+ * B: AACCGGTA -> AACCGGCA (D)
+ * We can see B can connect to 2 mutation, however, if A and B
+ * have the same mutation for C, they will have the same steps
+ * to go to the end, so that why whenever A get one mutation, we
+ * don't need to check it for B again, so we can delete C from the
+ * bank when C was found by A. Then we only check B for D. As a result,
+ * we continue checking for C and D and so on. Finally, we can simulate
+ * the analysis:
+ *       A(1) --> C(2)                 --> AA CC GC CA(3)     --> AA CC GG CA ==> len = 4
+ *      /
+ * start
+ *      \
+ *       B(1)     --> D(2) = AA CC GG CA (END) ==> len: 2
+ *
+ * So, the process must be run alterantely between A and B to continuously check the
+ * minimum steps, and then we come up with queue to do this process*/
