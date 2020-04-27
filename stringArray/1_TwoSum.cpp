11db4c018f3bd1b680d6121df28f2b9b3bae9aa9
Nhat Ho <honhattan2411@gmail.com>
2020-04-27 02:37:30 -0700

    Optimized Two sum problem

--- stringArray/1_TwoSum.cpp
+++ stringArray/1_TwoSum.cpp
@@ -1,3 +1,4 @@
+/* Brute Force, Time complexity O(n^2), Space O(1) */
 class Solution 
 {
 private:
@@ -18,5 +19,28 @@ public:
             }
         }
         return result;
+    }
+};
+
+/* Using map to reduce Time complexity to O(n), Space O(n) */
+class Solution
+{
+private:
+    vector<int>result;
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>res;
+        unordered_map<int,int>temp;//value, position
+        int remain;
+        for(int i = 0; i < nums.size(); i++){
+            remain = target-nums[i];
+            if(!temp.count(remain)) temp.insert({nums[i], i});
+            else{
+                res.push_back(i);
+                res.push_back(temp[remain]);
+                return res;
+            }
+        }
+        return res;
     }
 };
