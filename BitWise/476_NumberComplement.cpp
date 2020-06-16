da65035ac3c7a1dc719f7d1b2b48ea4b61ceb69f
NhatHo <honhattan2411@gmail.com>
2020-06-15 20:05:28 -0700

    Adding Number Complement problem

---
+++ BitWise/476_NumberComplement.cpp
@@ -0,0 +1,15 @@
+/* Example: 5 <=> 0000 0101 ==> ignore leading 0s ==> 101 ==> flipping it, we have: 010 <=> 2 */
+
+class Solution {
+public:
+    int findComplement(int num) {
+        int temp = 1, ret = 0;
+        while(num != 0){
+            //checking the last bit if it is 0 or 1, then flip it by using temp
+            if( (num & 1) == 0) ret |= temp; 
+            num >>=1;
+            temp <<=1;// temp is used to keep track the position of bit we are checking in num
+        }
+        return ret;
+    }
+};
