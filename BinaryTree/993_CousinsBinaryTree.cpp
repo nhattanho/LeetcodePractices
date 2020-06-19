c00fa616694f788b810e7702200cf758bb797098
NhatHo <honhattan2411@gmail.com>
2020-06-19 02:36:33 -0700

    Adding Cousins in Binary Tree Problem

---
+++ BinaryTree/993_CousinsBinaryTree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int depth1 = 0, depth2 = 0, flag = 0;
+    void depthFind(TreeNode* root, int x, int y, int level){
+        if(root == NULL) return;
+        if(root->left != NULL && root->right != NULL){
+            if( (root->left->val == x && root->right->val == y) 
+            || (root->left->val == y && root->right->val == x) ) {
+                flag = 1;
+                return;
+            }
+        }
+        level++;
+        if(root->val == x ) depth1 = level; //Each node has a unique integer value
+        if(root->val == y) depth2 = level;
+        depthFind(root->left, x, y, level);
+        depthFind(root->right, x, y, level);
+    }
+    
+    bool isCousins(TreeNode* root, int x, int y) {
+        int level = 0;
+        if(root == NULL) return false;
+        depthFind(root, x, y,level);
+        if(depth1 != depth2 || flag == 1) return false;
+        return true;
+    }
+};
