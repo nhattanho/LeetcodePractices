c70701c8694a117f11a0217be5c72626b48961f4
NhatHo <honhattan2411@gmail.com>
2020-09-16 12:49:37 -0700

    Create 2_CenturyFromYear.cpp

---
+++ CodeSignal/JourneyBegin/2_CenturyFromYear.cpp
@@ -0,0 +1,33 @@
+/* 
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100,
+the second - from the year 101 up to and including the year 200, etc.
+
+Example
+
+    For year = 1905, the output should be
+    centuryFromYear(year) = 20;
+    For year = 1700, the output should be
+    centuryFromYear(year) = 17.
+
+Input/Output
+
+    [execution time limit] 0.5 seconds (cpp)
+
+    [input] integer year
+
+    A positive integer, designating the year.
+
+    Guaranteed constraints:
+    1 ≤ year ≤ 2005.
+
+    [output] integer
+
+    The number of the century the year is in.
+*/
+
+int centuryFromYear(int year) {
+    int res = year/100;
+    int remainder = year%100;
+    if(res == 0) return 1;
+    else return res + ((remainder == 0)?0:1);
+}
