92a85c5cac47be1ddd381d921b3c85be5a9db142
NhatHo <honhattan2411@gmail.com>
2021-11-04 03:55:30 -0700

    Create 56_Merge_Intervals.cpp

---
+++ facebook/56_Merge_Intervals.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& inter) {
+        //sort(inter.begin(), inter.end(), [](vector<int>v1, vector<int>v2)-> bool { return v1[0] < v2[0];});
+        sort(inter.begin(), inter.end());
+        vector<vector<int>>ans;
+        vector<int>temp;
+        int start = inter[0][0], end = inter[0][1];
+        for (int i = 1; i < inter.size(); i++){
+            if (inter[i][0] > end) {
+                temp.push_back(start);
+                temp.push_back(end);
+                ans.push_back(temp);
+                temp.clear();
+                start = inter[i][0];
+                end = inter[i][1];
+                continue;
+            } else {
+                start = min(start, inter[i][0]);
+                end = max(end, inter[i][1]);
+            }
+            
+        }
+        temp.push_back(start);
+        temp.push_back(end);
+        ans.push_back(temp);
+        return ans;
+    }
+};
+
+auto speedup=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
