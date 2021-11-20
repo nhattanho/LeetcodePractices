5ee6f33b7b55ee3943221a8c622cc377a2480200
NhatHo <honhattan2411@gmail.com>
2021-11-20 04:51:21 -0800

    Create 392_Is_Subsequence.cpp

---
+++ Google/392_Is_Subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int left = 0;
+        
+        for(int i = 0; i < t.length(); i++){
+            if(t[i] == s[left]) left++;
+            if(left == s.length()) return true;
+        }
+        return left == s.length()?true:false;
+    }
+};
