e9c0875654ca40a4ff5d981cc908e190d5316b60
NhatHo <honhattan2411@gmail.com>
2021-12-25 20:47:15 -0800

    Update 88_Merge_Sorted_Array.cpp

--- Practice_Algorithm/88_Merge_Sorted_Array.cpp
+++ Practice_Algorithm/88_Merge_Sorted_Array.cpp
@@ -52,3 +52,27 @@ public:
         nums1 = res;
     }
 };
+
+/*Optimal solution
+Time O(n+m), space O(1)
+*/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int r1 = m-1, r2 = n-1, move = nums1.size()-1;
+        while(r1 >= 0 && r2 >= 0){
+            if(nums1[r1] <= nums2[r2]){
+                nums1[move] = nums2[r2];
+                r2--;
+            }else{
+                nums1[move] = nums1[r1];
+                r1--;
+            }
+            move--;
+        }
+        for(int i = r2; r2 >= 0; r2--){
+            nums1[move] = nums2[r2];
+            move--;
+        }
+    }
+};
