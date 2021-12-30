31eedc5c9c600dbf301a1d19fba55ef372e19731
NhatHo <honhattan2411@gmail.com>
2021-12-30 02:20:36 -0800

    Update 45_Jump_Game_II.cpp

--- Google/45_Jump_Game_II.cpp
+++ Google/45_Jump_Game_II.cpp
@@ -53,7 +53,7 @@ public:
         int size = nums.size();
         
         int check_jump = nums[0], farthest = 0, j = 0;
-        for(int i = 0; i < size-1; i++){
+        for(int i = 0; i < size-1; i++){/*less than size-1 since we dont want to check the end of array, just want to reach it, case check: [0] or [1,2]*/
             farthest = max(farthest, i+nums[i]);
             
             if(i == check_jump){
@@ -64,5 +64,24 @@ public:
         }
         /*case check_jump < size-1 <=> we will never reach the last index*/
         return (size == 1 || check_jump < size-1) ? 0:(j+1);
+    }
+};
+
+/*More readable*/
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int size = nums.size();
+        if(size == 1) return 0;
+        int check_jump = nums[0], farthest = 0, j = 0;
+        for(int i = 0; i < size-1; i++){
+            farthest = max(farthest, i+nums[i]);
+            
+            if(i == check_jump){
+                j++;
+                check_jump = farthest;
+            }
+        }
+        return (j+1);
     }
 };
