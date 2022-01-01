ab0b5f54a5e378374158939bf676e244058d5571
NhatHo <honhattan2411@gmail.com>
2022-01-01 02:50:31 -0800

    Update 231_Power_of_Two.cpp

--- BitWise/231_Power_of_Two.cpp
+++ BitWise/231_Power_of_Two.cpp
@@ -20,3 +20,20 @@ public:
     }
 };
 /*Time O(1)*/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0|| n<0) return false;
+        if(((~n+1) & n) == n) return true;
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0|| n<0) return false;
+        if((n&(n-1)) == 0) return true;
+        return false;
+    }
+};
