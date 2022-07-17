f613f362107268d11091fec19a04a75bf7158f35
NhatHo <nhatho@g.ucla.edu>
2022-07-17 01:46:38 -0700

    Create 724. Find Pivot Index.cpp

---
+++ Leetcode 75/724. Find Pivot Index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        for(auto i: nums){
+            sum += i;
+        }
+        int left = 0, right = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i == 0) left = 0;
+            else left += nums[i-1];
+            right = sum - nums[i] - left;
+            if(left == right) return i;
+        }
+        
+        return -1;
+    }
+};
