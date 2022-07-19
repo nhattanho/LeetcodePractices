e125fa02b7ca38b930d5f31e5ba8aa0862df7dec
NhatHo <nhatho@g.ucla.edu>
2022-07-18 20:20:38 -0700

    Update 392. Is Subsequence.cpp

--- Leetcode 75/392. Is Subsequence.cpp
+++ Leetcode 75/392. Is Subsequence.cpp
@@ -62,3 +62,16 @@ public:
         return s_point == s.length();
     }
 };
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int left = 0;
+        
+        for(int i = 0; i < t.length(); i++){
+            if(t[i] == s[left]) left++;
+            if(left == s.length()) return true;
+        }
+        return left == s.length()?true:false;
+    }
+};
