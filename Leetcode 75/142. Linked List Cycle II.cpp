783cbc6c96aeb415493dc31f056cd49b755ae791
NhatHo <nhatho@g.ucla.edu>
2022-07-25 10:23:16 -0700

    Create 142. Linked List Cycle II.cpp

---
+++ Leetcode 75/142. Linked List Cycle II.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head){
+        unordered_map<ListNode*,int>m;
+        while(head != NULL){
+            if(m.count(head)) return head;
+            m[head] = 0;
+            head = head->next;
+        }
+        return NULL; 
+    }
+};
+
+class Solution {
+public:
+    bool isCycle(ListNode *head, ListNode **remark){
+        while(head == NULL) return false;
+        ListNode *fast = head, *slow = head;
+        while(slow->next != NULL && fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow) {
+                *remark = fast;
+                return true;
+            };
+        }
+        return false;
+    }
+    
+    ListNode *detectCycle(ListNode *head){
+        ListNode *remark = NULL;
+        if(!isCycle(head, &remark)) return NULL;
+        ListNode *slow = head;
+        while(true){
+            if(slow == remark) return slow;
+            slow = slow->next;
+            remark = remark->next;
+        }
+        return NULL;   
+    }
+};
