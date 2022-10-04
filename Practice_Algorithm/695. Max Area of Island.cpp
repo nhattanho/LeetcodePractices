bf7ac354b01fea6c123ae655b9e5851af20188af
NhatHo <nhatho@g.ucla.edu>
2022-10-04 09:11:55 -0700

    Update 695. Max Area of Island.cpp

--- Practice_Algorithm/695. Max Area of Island.cpp
+++ Practice_Algorithm/695. Max Area of Island.cpp
@@ -58,3 +58,87 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int res = 0;
+        stack<vector<int>>st;
+        for(int r = 0; r < row; r++){
+            for(int c = 0; c <col; c++){
+                if(grid[r][c] == 1){
+                    st.push({r,c});
+                    grid[r][c] = 0;
+                    int count = 0;
+                    while(!st.empty()){
+                        vector<int>t = st.top();
+                        st.pop();
+                        count++;
+                        int r = t[0];
+                        int c = t[1];
+                        if(r-1>=0 && grid[r-1][c] != 0){
+                            st.push({r-1, c});
+                            grid[r-1][c] = 0;
+                        }
+                        if(r+1 < row && grid[r+1][c] != 0){
+                            st.push({r+1, c});
+                            grid[r+1][c] = 0;
+                        }
+                        if(c-1>=0 && grid[r][c-1] != 0){
+                            st.push({r, c-1});
+                            grid[r][c-1] = 0;
+                        }
+                        if(c+1 <col && grid[r][c+1]!=0){
+                            st.push({r, c+1});
+                            grid[r][c+1] = 0;
+                        }
+                    }
+                    res = max(res, count);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<int>>v = { {1,0},{-1,0},{0,1},{0,-1} };
+    //int v[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int res = 0;
+        stack<vector<int>>st;
+        for(int r = 0; r < row; r++){
+            for(int c = 0; c <col; c++){
+                if(grid[r][c] == 1){
+                    st.push({r,c});
+                    grid[r][c] = 0;
+                    int count = 0;
+                    while(!st.empty()){
+                        vector<int>t = st.top();
+                        st.pop();
+                        count++;
+                        int r = t[0];
+                        int c = t[1];
+                        
+                        for(auto x: v){
+                            int newr = r+x[0];
+                            int newc = c+x[1];
+                            if(newr >= 0 && newr < row && newc >= 0 && newc < col && grid[newr][newc] != 0) {
+                                st.push({newr, newc});
+                                grid[newr][newc] = 0;
+                            }
+                        }
+                    }
+                    res = max(res, count);
+                }
+            }
+        }
+        return res;
+    }
+};
