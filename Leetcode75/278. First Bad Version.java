5ed042c8d7c8001fb69645de12d761fc2d7b7734
NhatHo <nhatho@g.ucla.edu>
2022-10-16 19:28:48 -0700

    Create 278. First Bad Version.java

---
+++ Leetcode75/278. First Bad Version.java
@@ -0,0 +1,11 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+       int l = 1, r = n;
+        while(l < r){
+            int mid = l + (r-l)/2;
+            if(!isBadVersion(mid)) l = mid+1;
+            else r = mid;
+        }
+        return l;
+    }
+}
