953686185dc440efc700d1e07bfa613d240201ca
NhatHo <nhatho@g.ucla.edu>
2022-10-20 20:43:46 -0700

    Update 198. House Robber.cpp

--- Practice_Algorithm/198. House Robber.cpp
+++ Practice_Algorithm/198. House Robber.cpp
@@ -12,3 +12,19 @@ public:
         return nums[size-1];
     }
 };
+
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int len = nums.size();
+        if(len == 1) return nums[0];
+        int tmp = 0;
+        for(int i = 2; i < len; i++){
+            if(i == 2) nums[i] += nums[i-2];
+            else nums[i] = max(nums[i] + nums[i-2], nums[i]+nums[i-3]);
+        }
+        return max(nums[len-1], nums[len-2]);
+    }
+};
