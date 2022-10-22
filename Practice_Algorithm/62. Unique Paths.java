adddd189cb479434eb1dcfa50c8b006c1f3efdff
NhatHo <nhatho@g.ucla.edu>
2022-10-22 04:37:05 -0700

    Create 62. Unique Paths.java

---
+++ Practice_Algorithm/62. Unique Paths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][]dp = new int[m][n];
+        Arrays.stream(dp).forEach(a -> Arrays.fill(a, 1));
+        for(int r = m-2; r>= 0; r--){
+            for(int c = n-2; c>= 0; c--){
+                dp[r][c] = dp[r+1][c]+dp[r][c+1];
+            }
+        }
+        return dp[0][0];
+    }
+}
