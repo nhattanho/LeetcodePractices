d7a37173165427a25508225a27639a35702af5d0
NhatHo <nhatho@g.ucla.edu>
2022-11-03 01:44:50 -0700

    Update 183. Customers Who Never Order.sql

--- Practice_SQL/183. Customers Who Never Order.sql
+++ Practice_SQL/183. Customers Who Never Order.sql
@@ -4,3 +4,9 @@ where Customers.id not in
 (
     select Orders.customerid from Orders
 );
+
+
+select Customers.name as 'Customers'
+from Customers
+left join Orders on Customers.id = Orders.customerId
+where Orders.customerId is null;
