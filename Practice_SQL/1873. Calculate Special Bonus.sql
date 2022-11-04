0cf76f27fd4cc7d3b8284ee7015a75f63178410e
NhatHo <nhatho@g.ucla.edu>
2022-11-04 00:10:08 -0700

    Create 1873. Calculate Special Bonus.sql

---
+++ Practice_SQL/1873. Calculate Special Bonus.sql
@@ -0,0 +1,15 @@
+select 
+    employee_id,
+    if(employee_id % 2 != 0 and name not like 'M%', salary, 0) as bonus
+from Employees
+order by employee_id asc
+
+
+select 
+    employee_id,
+    case 
+        when employee_id%2!=0 and name not like 'M%' then salary
+        else 0
+    end as bonus
+from Employees
+order by employee_id asc
