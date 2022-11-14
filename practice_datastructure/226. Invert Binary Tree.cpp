e93310c0912b0ee5d380863d98a5eeb753c47d59
NhatHo <nhatho@g.ucla.edu>
2022-11-13 22:08:56 -0800

    Update 226. Invert Binary Tree.cpp

--- practice_datastructure/226. Invert Binary Tree.cpp
+++ practice_datastructure/226. Invert Binary Tree.cpp
@@ -16,3 +16,21 @@ public:
         return root;
     }
 };
+
+class Solution {
+public:
+    void dfs(TreeNode* root){
+        if(root == NULL) return;
+
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        dfs(root->left);
+        dfs(root->right);
+    }
+
+    TreeNode* invertTree(TreeNode* root) {
+        dfs(root);
+        return root;
+    }
+};
