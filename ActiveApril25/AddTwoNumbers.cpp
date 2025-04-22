35bc613ae205a476f029725ed264da5dd7d13d16
NhatHo <nhatho@g.ucla.edu>
2025-04-22 07:18:30 -0700

    Update AddTwoNumbers.cpp

--- ActiveApril25/AddTwoNumbers.cpp
+++ ActiveApril25/AddTwoNumbers.cpp
@@ -13,13 +13,13 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode *res = new ListNode();
         ListNode *run = res;
-        int remain = 0;
-        while(l1 != NULL || l2 != NULL || remain != 0) 
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL || carry != 0) 
         {
             int num1 = l1 ? l1->val : 0;
             int num2 = l2 ? l2->val : 0;
             int sum = num1 + num2 + remain;
-            remain = sum/10;
+            carry = sum/10;
             sum %= 10;
             run->next = new ListNode(sum);
             run = run->next;
@@ -27,6 +27,35 @@ public:
             l2 = l2 ? l2->next : NULL;
         }
         res = res->next;
+        return res;
+    }
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *res = NULL;
+        ListNode *run = res;
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL || carry != 0)
+        {
+            int num1 = l1 ? l1->val : 0;
+            int num2 = l2 ? l2->val : 0;
+            int sum = num1 + num2 + carry;
+            carry = sum/10;
+            sum %= 10;
+            ListNode *temp = new ListNode(sum);
+            if(run == NULL) {
+                run = temp;
+                res = run;   
+            }
+            else {
+                run->next = temp;
+                run = temp;   
+            }
+            l1 = l1? l1->next : NULL;
+            l2 = l2 ? l2->next : NULL;
+        }
         return res;
     }
 };
