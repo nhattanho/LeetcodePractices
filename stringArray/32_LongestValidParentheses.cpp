0e2ab8d2b5fdb16a4857742bd07f8638dc2b19f4
NhatHo <nhatho@g.ucla.edu>
2025-09-14 16:58:46 -0700

    Update 32_LongestValidParentheses.cpp

--- stringArray/32_LongestValidParentheses.cpp
+++ stringArray/32_LongestValidParentheses.cpp
@@ -1,3 +1,9 @@
+// For example, we have ) ( ) ( ( ) ) ) 
+//                index 0 1 2 3 4 5 6 7
+// and then if we reduce the well-formed parentheses, the string will look like ) )
+//                                                                              0 7
+// then zero will be the first base to calculate the len, which is 6 - 0 = 6, then 7 will be the 
+// next base for the next well-formed ones.
 class Solution {
 public:
     int longestValidParentheses(string s) {
@@ -17,6 +23,42 @@ public:
                 }
             }
         }  
+        return maxlen;
+    }
+};
+
+// Optimize solution with no extra memory by using two scans to cover all well-formed parentheses
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int len = s.length();
+        int left = 0, right = 0;
+        int maxlen = 0;
+
+        // The first scanning from left to right will miss cases such as: ( ( ( ( ( ) ) ()
+        // However, if we start doing similar scanning from right to left, these cases will be covered.
+        for (int i = 0; i < len; i++){
+            if(s[i] == '(') left++;
+            else right++;
+
+            if(left == right)
+                maxlen = max(maxlen, 2*right);
+            else if(right > left){ // meet the base close and start the new well-formed
+                right = 0;
+                left = 0;
+            }
+        }
+        left = 0, right = 0;
+        for(int i = len-1; i >= 0; i--){
+            if(s[i] == ')') right++;
+            else left ++;
+
+            if(right == left) maxlen = max(maxlen, 2*left);
+            else if(left > right){ // meet the base open and start the new well-formed
+                left = 0;
+                right = 0;
+            }
+        }
         return maxlen;
     }
 };
